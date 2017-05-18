8aa603ed9e7b9af16e523bb7da211ac40c995c3a
Khoi Tran <humar104@gmail.com>
2017-05-17 22:14:54 -0600

    Filling out client class, basic client class functions

--- dcss/client.py
+++ dcss/client.py
@@ -1,33 +1,35 @@
-import connection as con
-import fake_terminal as ft
+import sys
+
+from .connection import Connection
+from .terminal_buffer import TerminalBuffer
+from .player import Player
+from .inventory import Inventory
+from .map import Map
+from .spells import Spells
 
 
 class Client:
     def __init__(self, crawlConnection):
         self.conn = crawlConnection
-        self.screen = ft.FakeTerminal()
+        self.screen = TerminalBuffer()
         if(self.conn and not self.conn.validConnection):
             if(self.conn.connect()):
                 self.screen.input(self.conn.crawl_login())
 
-    def send_command(self, command, addNewLine=False):
-        s = self.conn.send_command(command, addNewLine)
-        self.screen.input(s)
-        return s
-
-def test(username, pw):
-    client = Client(con.RemoteCC(username, pw))
-    if not client.conn.validConnection:
+    def remote_connect(self, sshUsername, sshPassword):
         return
-    f = open("testFT.txt", "w")
-    print("Begin input...")
-    while True:
-        i = input()
-        f.write("new input:" + i + "\n")
-        s = client.send_command(i)
-        f.write("new text:" + s + "\n")
-        f.write("new screen:\n" + str(client.screen) + "\n")
-        print(str(client.screen))
 
-def test2(username, pw):
-    return Client(con.RemoteCC(username, pw))
+    def remote_login(self, crawlUsername, crawlPassword):
+        return
+
+    def local_connect(self):
+        return
+
+    def local_login(self):
+        return
+
+
+if __name__ == '__main__':
+    client = Client(Connection.RemoteCC(sys.argv[1], sys.argv[2]))
+    if not client.conn.validConnection:
+        sys.exit(0)
