f2d3284ef59c6a336c0f4998c10f0cfe6b9bf4e2
Lukas Hillmer <lukas.hillmer@gmail.com>
2017-06-11 21:12:26 -0400

    pep8 + var renaming

--- dcss/terminal_buffer.py
+++ dcss/terminal_buffer.py
@@ -112,11 +112,11 @@ class TerminalBuffer():
     def __init__(self, width=80, height=24, ignoreUnsupported=True):
         self.width = width
         self.height = height
-        self.windowTop = 0
-        self.windowBottom = height - 1
+        self.window_top = 0
+        self.window_bot = height - 1
         # if we find an escape sequence that's not supported
         # this dictates whether the terminal errors, or silently ignores it
-        self.ignoreUnsupported = ignoreUnsupported
+        self.skip_unsupported = ignoreUnsupported
 
         # this is 'backwards' from normal
         # (i.e. to get the char at x,y you do self.terminal[y][x])
@@ -124,12 +124,12 @@ class TerminalBuffer():
         self.terminal = [
             [self.Character("", None) for _ in range(self.width)]
             for __ in range(self.height)]
-        self.cursorPosition = self.Position(0, 0)
+        self.cursor_pos = self.Position(0, 0)
         self.savedCursorPosition = self.Position(-1, -1)
 
         # this represents default coloring,
         # so set it as the current color from the start
-        self.currentColor = EscapeSequence([0], "m")
+        self.cur_color = EscapeSequence([0], "m")
 
     def __str__(self):
         return self.get_text()
@@ -182,7 +182,7 @@ class TerminalBuffer():
             else:
                 # handle special case characters
                 if val == '\r':
-                    self.cursorPosition.x = 0
+                    self.cursor_pos.x = 0
                 elif val == '\b':
                     self.move_cursor(-1, 0, True)
                 elif val == '\n':
@@ -191,9 +191,8 @@ class TerminalBuffer():
                     pass
                 # standard characters
                 else:
-                    self.terminal[self.cursorPosition.y] \
-                        [self.cursorPosition.x] \
-                        = self.Character(val, self.currentColor)
+                    self.terminal[self.cursor_pos.y][self.cursor_pos.x] \
+                        = self.Character(val, self.cur_color)
                     self.move_cursor(1, 0, True)
 
         if len(log_dict):
@@ -215,59 +214,59 @@ class TerminalBuffer():
     def move_cursor(self, x, y, wrap):
         if wrap:
             # handles moving 'too far' forward or back
-            while self.cursorPosition.x + x >= self.width:
+            while self.cursor_pos.x + x >= self.width:
                 y += 1
                 x -= self.width
-            while self.cursorPosition.x + x < 0:
+            while self.cursor_pos.x + x < 0:
                 x += self.width
                 y -= 1
-        if self.cursorPosition.y + y >= self.windowBottom:
-            self.scroll_up((self.cursorPosition.y + y) -
-                           (self.windowBottom - 1))
-            y = (self.cursorPosition.y + y) - (self.windowBottom - 1)
+        if self.cursor_pos.y + y >= self.window_bot:
+            self.scroll_up((self.cursor_pos.y + y) -
+                           (self.window_bot - 1))
+            y = (self.cursor_pos.y + y) - (self.window_bot - 1)
         # technically, shouldn't need the max/min functions here
         # but they aren't very expensive, and being safe is cool
-        self.cursorPosition.x = max(
-            0, min(self.width - 1, self.cursorPosition.x + x))
-        self.cursorPosition.y = max(
-            0, min(self.height - 1, self.cursorPosition.y + y))
+        self.cursor_pos.x = max(
+            0, min(self.width - 1, self.cursor_pos.x + x))
+        self.cursor_pos.y = max(
+            0, min(self.height - 1, self.cursor_pos.y + y))
 
     def clear_terminal(self):
         # this also needs to reset cursor position
-        self.cursorPosition.x = 0
-        self.cursorPosition.y = 0
+        self.cursor_pos.x = 0
+        self.cursor_pos.y = 0
         for i in range(len(self.terminal)):
             for k in range(len(self.terminal[i])):
                 self.terminal[i][k].reset()
 
     def clear_from_start(self):
-        for i in range(self.cursorPosition.x):
-            for j in range(self.cursorPosition.y):
+        for i in range(self.cursor_pos.x):
+            for j in range(self.cursor_pos.y):
                 self.terminal[j][i].reset()
 
     def clear_to_end(self):
-        for i in range(self.cursorPosition.x, self.width):
-            for j in range(self.cursorPosition.y, self.height):
+        for i in range(self.cursor_pos.x, self.width):
+            for j in range(self.cursor_pos.y, self.height):
                 self.terminal[j][i].reset()
 
     def clear_line_before(self):
-        for i in range(self.cursorPosition.x):
-            self.terminal[self.cursorPosition.y][i].reset()
+        for i in range(self.cursor_pos.x):
+            self.terminal[self.cursor_pos.y][i].reset()
 
     def clear_line_after(self):
-        for i in range(self.cursorPosition.x, self.width):
-            self.terminal[self.cursorPosition.y][i].reset()
+        for i in range(self.cursor_pos.x, self.width):
+            self.terminal[self.cursor_pos.y][i].reset()
 
     def clear_line(self):
         for i in range(self.width):
-            self.terminal[self.cursorPosition.y][i].reset()
+            self.terminal[self.cursor_pos.y][i].reset()
 
     def scroll_up(self, num):
-        for i in range(self.windowTop, self.windowBottom + 1):
+        for i in range(self.window_top, self.window_bot + 1):
             # we are shifting num lines
             # for any line that's more than num lines from the bottom
             # just copy from the line num lines down
-            if i < self.windowBottom - num:
+            if i < self.window_bot - num:
                 self.terminal[i] = self.terminal[i + num]
             # if the line is within num lines of the bottom, reset the line
             else:
@@ -277,11 +276,11 @@ class TerminalBuffer():
     def scroll_down(self, num):
         # we need to iterate backwards here
         # otherwise, we just copy the first num lines over and over again
-        for i in range(self.windowBottom, self.windowTop + 1, -1):
+        for i in range(self.window_bot, self.window_top + 1, -1):
             # we are shifting num lines
             # for any line that's more than num lines from the bottom
             # just copy from the line num lines down
-            if i >= num + self.windowBottom:
+            if i >= num + self.window_bot:
                 self.terminal[i] = self.terminal[i - num]
             # if the line is within num lines of the bottom, reset
             else:
@@ -292,14 +291,14 @@ class TerminalBuffer():
         # this is effectively deleting characters
         # and pushing the remaining characters back, due to the removed chars
         # we can just move the characters, overwriting the old values
-        for i in range(self.cursorPosition.x, self.width):
+        for i in range(self.cursor_pos.x, self.width):
             # move characters that aren't being deleted
             if i + amount < self.width:
-                self.terminal[self.cursorPosition.y][i] = \
-                    self.terminal[self.cursorPosition.y][i + amount]
+                self.terminal[self.cursor_pos.y][i] = \
+                    self.terminal[self.cursor_pos.y][i + amount]
             # clear characters if the source would be outside the screen
             else:
-                self.terminal[self.cursorPosition.y][i] = \
+                self.terminal[self.cursor_pos.y][i] = \
                     self.Character("", None)
 
     def apply_sequence(self, sequence, log_dict):
@@ -312,23 +311,23 @@ class TerminalBuffer():
         elif sequence.sequenceType == SequenceType.CURSOR_BACK:
             self.move_cursor(-sequence.get_data(0), 0, False)
         elif sequence.sequenceType == SequenceType.CURSOR_NEXT_LINE:
-            self.move_cursor(-self.cursorPosition.x,
+            self.move_cursor(-self.cursor_pos.x,
                              sequence.get_data(0), False)
         elif sequence.sequenceType == SequenceType.CURSOR_PREVIOUS_LINE:
-            self.move_cursor(-self.cursorPosition.x, -
+            self.move_cursor(-self.cursor_pos.x, -
                              sequence.get_data(0), False)
         elif sequence.sequenceType == SequenceType.CURSOR_HORIZONTAL_ABSOLUTE:
-            self.cursorPosition.x = max(
+            self.cursor_pos.x = max(
                 0, min(self.width - 1, sequence.get_data(0) - 1))
         elif sequence.sequenceType == SequenceType.CURSOR_VERTICAL_ABSOLUTE:
-            self.cursorPosition.y = max(
+            self.cursor_pos.y = max(
                 0, min(self.height - 1, sequence.get_data(0) - 1))
         elif sequence.sequenceType == \
                 SequenceType.HORIZONTAL_VERTICAL_POSITION \
                 or sequence.sequenceType == SequenceType.CURSOR_POSITION:
-            self.cursorPosition.x = max(
+            self.cursor_pos.x = max(
                 0, min(self.width - 1, sequence.get_data(1) - 1))
-            self.cursorPosition.y = max(
+            self.cursor_pos.y = max(
                 0, min(self.height - 1, sequence.get_data(0) - 1))
         elif sequence.sequenceType == SequenceType.ERASE_IN_DISPLAY:
             if sequence.get_data(0) == 2:
@@ -351,18 +350,18 @@ class TerminalBuffer():
         elif sequence.sequenceType == SequenceType.SCROLL_DOWN:
             self.scroll_down(sequence.get_data(0))
         elif sequence.sequenceType == SequenceType.SELECT_GRAPHIC_RENDITION:
-            self.currentColor = sequence
+            self.cur_color = sequence
         elif sequence.sequenceType == SequenceType.SAVE_CURSOR_POSITION:
-            self.savedCursorPosition.x = self.cursorPosition.x
-            self.savedCursorPosition.y = self.cursorPosition.y
+            self.savedCursorPosition.x = self.cursor_pos.x
+            self.savedCursorPosition.y = self.cursor_pos.y
         elif sequence.sequenceType == SequenceType.RESTORE_CURSOR_POSITION:
             if self.savedCursorPosition.x != -1:
-                self.cursorPosition.x = self.savedCursorPosition.x
-                self.cursorPosition.y = self.savedCursorPosition.y
+                self.cursor_pos.x = self.savedCursorPosition.x
+                self.cursor_pos.y = self.savedCursorPosition.y
         elif sequence.sequenceType == SequenceType.SET_WINDOW:
             if sequence.get_data(0) == 0 and sequence.get_data(1) == 0:
-                self.windowTop = 0
-                self.windowBottom = self.height - 1
+                self.window_top = 0
+                self.window_bot = self.height - 1
             else:
                 if sequence.get_data(0) - 1 > self.height or\
                         sequence.get_data(0) - 1 < 0 or\
@@ -370,9 +369,9 @@ class TerminalBuffer():
                         sequence.get_data(1) < sequence.get_data(0):
                     log.warning("ignoring windowSize: " + repr(sequence))
                 else:
-                    self.windowTop = sequence.get_data(0) - 1
-                    self.windowBottom = sequence.get_data(1) - 1
-        elif self.ignoreUnsupported:
+                    self.window_top = sequence.get_data(0) - 1
+                    self.window_bot = sequence.get_data(1) - 1
+        elif self.skip_unsupported:
             if str(sequence) in log_dict.keys():
                 log_dict[str(sequence)] += 1
             else:
